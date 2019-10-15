cb62d632c211927130be7c46fe054dacfeac0e0f
andyprasetya <andyprasetya@opense7en.com>
2019-10-15 13:13:29 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import '../node_modules/spectre.css/src/spectre.scss';
+import '../node_modules/spectre.css/src/spectre-icons.scss';
+import '../node_modules/spectre.css/src/spectre-exp.scss';
+import '../node_modules/leaflet/dist/leaflet.css';
+import './stylesheet/custom.css';
+import './stylesheet/font-awesome.min.css';
+// UI entry point
+import commonui from './ui';
+// App's UI initialization
+commonui.initUI('app');
